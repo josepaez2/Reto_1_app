9e52a8409d6cc1b629e7ae0e7802c675c6432c13
josepaez2 <josepaez_2@hotmail.com>
2018-01-17 17:23:44 -0500

    Adds Controller and Routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,16 @@
 class PagesController < ApplicationController
+  def home
+
+  end
+
+  def static_page_1
+  	
+  end
+
+  def static_page_2
+  	
+  end
+  def static_page_3
+  	
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+  # resources :pages
+  get '/ruby', to: 'pages#static_page_1', as: "Static_Page_1"
+  get '/git', to: 'pages#static_page_2', as: "Static_Page_2"
+  get '/git', to: 'pages#static_page_3', as: "Static_Page_3"
 end
